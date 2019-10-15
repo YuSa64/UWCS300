48b94c390eb4023d04bd45db35bc2d9993c818a6
korYuSa <rtgh64@gmail.com>
2019-10-14 19:03:57 -0500

    191014

---
+++ workspace/P05 Memeage 5000/src/ColorPlusChar.java
@@ -0,0 +1,17 @@
+
+public class ColorPlusChar {
+  public ColorPlusChar(Color color, char character) {
+    
+  }
+  public ColorPlusChar(Color color) {
+    
+  }
+  // stores 8-bit character within the least significant bits of color components
+  public void hideChar(char character) {
+    
+  }
+  // retrieves 8-bit character from the least significant bits of color components
+  public char revealChar() {
+    return '';
+  }
+}
--- workspace/P05 Memeage 5000/src/MemeageTests.java
+++ workspace/P05 Memeage 5000/src/MemeageTests.java
@@ -1,13 +1,17 @@
+import java.io.File;
+import java.io.IOException;
 
 public class MemeageTests {
   public static void main(String[] args) {
     System.out.println(testFourBytesGetBits());
     System.out.println(testFourBytesSetBits());
     System.out.println(testColor());
+    System.out.println(testImage());
   }
 
   public static boolean testFourBytesGetBits() {
-    FourBytes test = new FourBytes(Integer.parseInt("11010000000000", 2)); //bitstring: 110100 00000000
+    FourBytes test = new FourBytes(Integer.parseInt("11010000000000", 2)); // bitstring:
+                                                                           // 110100 00000000
     if (test.getBits(14, 0) == 13312)
       return true;
     else
@@ -15,7 +19,7 @@ public class MemeageTests {
   }
 
   public static boolean testFourBytesSetBits() {
-    FourBytes test = new FourBytes(0); //bitstring: 000 00000000
+    FourBytes test = new FourBytes(0); // bitstring: 000 00000000
     test.setBits(3, 8, 5); // bitstring: 101 00000000
     if (test.getBits(11, 0) == 1280)
       return true;
@@ -24,18 +28,30 @@ public class MemeageTests {
   }
 
   public static boolean testColor() {
-    Color test = new Color(255, 224, 255, 255); //bitstring: 1111111 11100000 11111111 11111111
+    Color test = new Color(255, 224, 255, 255); // bitstring: 1111111 11100000 11111111 11111111
     // which is equal to -2031617
-    System.out.println((int)Long.parseLong("11111111111000001111111111111111", 2));
-    if(test.getARGB() != -2031617)
+    if (test.getARGB() != -2031617)
       return false;
-    
+    test.setARGB(-1073807105); //bitstring: 10111111 11111111 00000000 11111111
+    if (test.getAlpha() != 191 || test.getRed() != 255 || test.getGreen() != 0
+        || test.getBlue() != 255)
+      return false;
     return true;
 
   }
 
   public static boolean testImage() {
-
-    return false;
+    File image = new File("testImage.png");
+    Image test;
+    try {
+      test = new Image(image);
+      if(test.getHeight() !=3 || test.getWidth() != 3)
+        return false;
+      if(test.getColor(1, 1).getARGB() != -16711681)
+        return false;
+      return true;
+    } catch (IOException e) {
+      return false;
+    } 
   }
 }
